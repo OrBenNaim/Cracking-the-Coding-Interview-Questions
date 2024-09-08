64610f34cb06ef8b7dd3572d3c9c817558962df9
Or Ben Naim <orbennaim123@gmail.com>
2024-09-08 13:28:02 +0300

    Q.A (1.2)

--- C and C++ exercises/Cracking the Code Interview Questions.cpp
+++ C and C++ exercises/Cracking the Code Interview Questions.cpp
@@ -5,12 +5,31 @@
 
 using namespace std;
 
-int isUnique(char* s);	// (1.1) Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+// (1.1) Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+int isUnique(char* s);
+void test_isUnique();
+
+// (1.2) Check Permutation: Given two strings, write a method to decide if one is a permutation of the other. 
+bool CheckPermutation(const string& s1, const string& s2); 
+void test_CheckPermutation();
 
 
 int main()
 {
-	char str[] = "abbbcd";
+	test_isUnique();	
+	test_CheckPermutation();
+
+
+	return 0;
+}
+
+
+void test_isUnique()
+{
+	char str[] = "abcdd";
+
+	printf("\nOutput of Question 1.1:");
+
 	if (isUnique(str))
 	{
 		printf("\n%s has all unique characters\n", str);
@@ -19,8 +38,9 @@ int main()
 	{
 		printf("\n%s hasn't all unique characters\n", str);
 	}
-	return 0;
+	printf("\n");
 }
+
 
 int isUnique(char* s)
 {
@@ -40,3 +60,26 @@ int isUnique(char* s)
 	}
 	return 1;
 }
+
+
+void test_CheckPermutation()
+{
+	string str1 = "abc", str2 = "cba";
+
+	cout << "\nOutput of Question 1.2:" << endl ;
+
+
+	cout << "\n" << endl ;
+}
+
+
+bool CheckPermutation(const string &s1, const string &s2)
+{
+    int sum1 = 0, sum2 = 0;
+
+
+
+	return (sum1 == sum2) ? true : false;
+}
+
+
