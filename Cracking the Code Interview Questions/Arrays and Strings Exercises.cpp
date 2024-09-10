ba40dd20ae6c1cb2be9fc4910647b847acbc08bf
Or Ben Naim <orbennaim123@gmail.com>
2024-09-10 22:52:10 +0300

    upload

--- Cracking the Code Interview Questions/Arrays and Strings Exercises.cpp
+++ Cracking the Code Interview Questions/Arrays and Strings Exercises.cpp
@@ -34,6 +34,17 @@ bool permutationPalindrome(const string& s);
 void test_permutationPalindrome();
 
 
+/* (1.5) One Away: There are three types of edits that can be performed on strings: insert a character, remove a character, or replace a character.
+Given two strings, write a function to check if they are one edit (or zero edits) away.
+EXAMPLE
+pale, ple -> true
+pales, pale -> true
+pale, bale -> true
+pale, bake -> false 
+*/
+
+
+
 int main()
 {
 	// When you declare a function pointer in C++, you use a syntax like this: void (*funcPtr)();
